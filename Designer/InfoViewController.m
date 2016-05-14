f478ef857688ddcf7e34691b5e4550472123db41
SincereD <sincere129120@icloud.com>
2016-05-14 21:59:09 +0800

    修改关于我们页面

--- Designer/InfoViewController.m
+++ Designer/InfoViewController.m
@@ -13,6 +13,9 @@
 @interface InfoViewController ()<UIGestureRecognizerDelegate,UINavigationControllerDelegate>
 {
     CGFloat _height;
+    InfoView * copyrightView;
+    InfoView * designerView;
+    InfoView * coderView;
 }
 @end
 
@@ -28,6 +31,8 @@
 {
     [super viewDidAppear:animated];
     [self initSences];
+    self.navigationController.delegate = self;
+    self.navigationController.interactivePopGestureRecognizer.delegate = self;
 }
 
 - (void)initBackBtn
@@ -39,9 +44,6 @@
     [returnButtonItem setTarget:self];
     [returnButtonItem setTintColor:[UIColor brownColor]];
     self.navigationItem.leftBarButtonItem = returnButtonItem;
-    
-    self.navigationController.delegate = self;
-    self.navigationController.interactivePopGestureRecognizer.delegate = self;
 }
 
 -(void)navigationController:(UINavigationController *)navigationController didShowViewController:(UIViewController *)viewController animated:(BOOL)animated
@@ -60,9 +62,13 @@
 - (void)initSences
 {
     _height = (kScreenHeight - 64.0f)/3.0f;
-    [self copyright];
-    [self coder];
-    [self designer];
+    [self.view setClipsToBounds:YES];
+    if (!copyrightView)
+    {
+        [self copyright];
+        [self coder];
+        [self designer];
+    }
 }
 
 - (void)copyright
@@ -70,7 +76,7 @@
     InfoData * copyrightData = [[InfoData alloc] initWithPhotoName:@"copyright.png"
                                                   mailBox:@"copyright"
                                                   subject:@"动效之美v1.0"];
-    InfoView * copyrightView = [[InfoView alloc] initWithData:copyrightData frame:CGRectMake(-kScreenWidth, 64, kScreenWidth, _height)];
+    copyrightView = [[InfoView alloc] initWithData:copyrightData frame:CGRectMake(-kScreenWidth, 64, kScreenWidth, _height)];
     [self.view addSubview:copyrightView];
     
     [UIView animateWithDuration:0.8 animations:^{
@@ -83,7 +89,7 @@
     InfoData * designerData = [[InfoData alloc] initWithPhotoName:@"designer.png"
                                                            mailBox:@"yu.yi.fan@foxmail.com"
                                                            subject:@"设计师"];
-    InfoView * designerView = [[InfoView alloc] initWithData:designerData
+    designerView = [[InfoView alloc] initWithData:designerData
                                                        frame:CGRectMake(kScreenWidth, _height + 64, kScreenWidth, _height)];
     [self.view addSubview:designerView];
     [UIView animateWithDuration:0.8 animations:^{
@@ -96,7 +102,7 @@
     InfoData * coderData = [[InfoData alloc] initWithPhotoName:@"coder"
                                                        mailBox:@"sincere129120@icloud.com"
                                                        subject:@"开发者"];
-    InfoView * coderView = [[InfoView alloc] initWithData:coderData
+    coderView = [[InfoView alloc] initWithData:coderData
                                                     frame:CGRectMake(-kScreenWidth, 64 + _height * 2.0f, kScreenWidth, _height)];
     [self.view addSubview:coderView];
     
