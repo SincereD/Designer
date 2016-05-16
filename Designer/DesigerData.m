ae431d9b7a22c12839931b64ecb7a1053767c08a
SincereD <sincere129120@icloud.com>
2016-05-16 22:45:47 +0800

    新建设计师数据模型

---
+++ Designer/DesigerData.m
@@ -0,0 +1,13 @@
+//
+//  DesigerData.m
+//  Designer
+//
+//  Created by Sincere on 16/5/14.
+//  Copyright © 2016年 Sincere. All rights reserved.
+//
+
+#import "DesigerData.h"
+
+@implementation DesigerData
+
+@end
